07f739311be2c05b0b79ae40c465b45826c04246
Ankit Baid <ankitbaid11326@gmail.com>
2017-05-16 15:32:14 +0530

    User class added with instantation method for all users no need to define users manually

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -8,14 +8,31 @@
 				<small> SubHeading </small>
 			</h1>
 
-			<?php 
-				
-				$sql = "SELECT * FROM users WHERE id=1";
-				$result = $database->query($sql);
-				$user_found = mysqli_fetch_array($result);
+			<?php
 
-				echo $user_found['username'];
-					
+				// $result_set = User::find_all_users();
+				//
+				// while($row = mysqli_fetch_array($result_set)){
+				// 	echo $row['username']."<br/>";
+				// }
+
+				// $found_user = User::find_user_by_id(1);
+				//
+				// $user = User::instantation($found_user);
+				//
+				// echo $user->username;
+
+				// $users = User::find_all_users();
+				//
+				// foreach ($users as $user) {
+				// 	echo $user->id . "<br/>";
+				// }
+
+				$found_user = User::find_user_by_id(1);
+
+				echo $found_user->username;
+
+
 			?>
 
 
--- admin/includes/new_config.php
+++ admin/includes/new_config.php
@@ -3,9 +3,9 @@
 // Database Connection Constants
 
 define('DB_HOST','localhost');
-define('DB_USER','root'); 
-// define('DB_PASS',''); // this one is for mac
-define('DB_PASS','qwertyuiop'); // this one is for windows home
+define('DB_USER','root');
+define('DB_PASS',''); // this one is for mac
+// define('DB_PASS','qwertyuiop'); // this one is for windows home
 define('DB_NAME','gallery_db');
 
 
