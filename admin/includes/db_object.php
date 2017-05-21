1e3a4d6cdfb1b439a848ffeb0a4426b335d58ced
Ankit Baid <Ankit Baid>
2017-05-22 01:42:24 +0530

    DB_Object class added

---
+++ admin/includes/db_object.php
@@ -0,0 +1,54 @@
+<?php
+
+class DB_Object{
+
+    public static function find_all(){
+
+        return self::find_this_query("SELECT * FROM ". self::$db_table. " ");
+
+    }
+
+    public static function find_by_id($user_id){
+
+        $the_result_array =  self::find_this_query("SELECT * FROM ". self::$db_table. " WHERE id = $user_id LIMIT 1");
+
+        return !empty($the_result_array) ? array_shift($the_result_array) : false;
+
+        return $found_array;
+    }
+
+    public static function find_this_query($sql){
+        global $database;
+
+        // echo $sql;
+
+        $result_set = $database->query($sql);
+        $the_object_array = array();
+
+        while($row = mysqli_fetch_array($result_set)){
+            $the_object_array[] = self::instantation($row);
+        }
+
+        return $the_object_array;
+    }
+
+    public static function instantation($the_record){
+        $the_object = new self;
+
+        foreach ($the_record as $the_attribute => $value) {
+            if($the_object->has_the_attribute($the_attribute)){
+                $the_object->$the_attribute = $value;
+            }
+        }
+
+        return $the_object;
+    }
+
+    private function has_the_attribute($the_attribute){
+        $object_properties = get_object_vars($this);
+        return array_key_exists($the_attribute, $object_properties);
+    }
+
+}
+
+?>
